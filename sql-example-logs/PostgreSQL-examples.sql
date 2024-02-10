049a8fab8c8d02e22415084d5ce1855a650ddb7f
Stann-Omar Jones <35823259+k10sj02@users.noreply.github.com>
2024-02-10 16:24:54 -0500

    Update PostgreSQL-examples.sql

--- sql-example-logs/PostgreSQL-examples.sql
+++ sql-example-logs/PostgreSQL-examples.sql
@@ -1,8 +1,8 @@
-Has anyone ever made a cohort analysis pivot table on a quarterly rather than monthly basis?
+"Has anyone ever made a cohort analysis pivot table on a quarterly rather than monthly basis?
 I'm working on that in SQL (MySQL 5.7.12) and I can get each of the quarters (Q2 '23, Q3 '23, etc.) 
 in the column label, but I'm actually wondering now if each of the instances across the row headers should represent billing months or billing quarters
 The only reason I'm making this quarterly cohort analysis anyway is because a client asked for it 
-in addition to the regular monthly cohort analysis?
+in addition to the regular monthly cohort analysis?"
 
 SELECT
 concat('Q',ceil(month(fdf.delivered_at::date)/3),'-',year(fdf.delivered_at::date)) as quarter_occurred,
